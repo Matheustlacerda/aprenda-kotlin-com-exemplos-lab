047f1409b0c1a761400848b1272ff6f5bc8731a2
Matheustlacerda <64924139+Matheustlacerda@users.noreply.github.com>
2023-10-05 14:46:22 -0300

    Desafio finalizado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BÁSICO, INTERMEDIARIO, AVANÇADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String, val id: Int)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, val duracao: Int, var level: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var level: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun show_user(){
+        for(user in inscritos){
+            println("Id: ${user.id} - ${user.nome}")
+        }
+    }
+
+    fun show_content(){
+        for(content in conteudos){
+            println("Nome da linguagem de programação: ${content.nome}, Carga Horária: ${content.duracao}, Dificuldade: ${content.level}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Python",50,Nivel.BÁSICO)
+    val conteudo2 = ConteudoEducacional("Kotlin",100,Nivel.AVANÇADO)
+    val conteudo3 = ConteudoEducacional("Ruby",80,Nivel.INTERMEDIARIO)
+    val usuario1 = Usuario("Matheus",1)
+    val usuario2 = Usuario("Eduardo",2)
+
+    val grade = mutableListOf<ConteudoEducacional>()
+        grade.add(conteudo1)
+        grade.add(conteudo2)
+        grade.add(conteudo3)
+
+    
+    val escola = Formacao("Escola de Programação",grade,Nivel.AVANÇADO )
+
+    escola.matricular(usuario1)
+    escola.matricular(usuario2)
+
+    println("\nAlunos Matriculados:")
+    escola.show_user()
+   
+    println("\nLinguagens de Programação disponíveis:")
+    escola.show_content()
 }
